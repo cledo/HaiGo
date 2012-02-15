206764ba9da4d73c6133607a813d8198db9b50bd
Clemens Dorner <clemens.dorner@gmail.com>
2012-02-15 16:22:06 +0100

    more sgf parsing

--- src/sgf.c
+++ src/sgf.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>  // remove when printf is not needed anymore
 #include <ctype.h>
+#include <stdbool.h>
 #include "global_const.h"
 #include "sgf.h"
 
@@ -18,10 +19,15 @@
 void parse_sgf( char *file_content )
 {
     int  k = 0;
+    int  l = 0;
     char current_char;
     char last_char;
     int  game_tree_level = INVALID;
     int  node_nr         = INVALID;
+    bool in_PropValue    = false;
+    char PropValue[128];
+    bool in_PropIdent    = false;
+    char PropIdent[128];
 
     while ( ( current_char = file_content[k++] ) != '\0' ) {
         if ( current_char == '(' ) {
@@ -36,11 +42,39 @@ void parse_sgf( char *file_content )
             node_nr++;
             printf( "#### New Node %d\n", node_nr );
         }
-        if ( isupper(current_char) && ( isspace(last_char) || last_char == ']' || last_char == ';' ) ) {
-            printf( "    ## New Property\n" );
+        if ( isupper(current_char) && ( isspace(last_char) || (last_char == ']') || (last_char == ';') ) ) {
+            in_PropIdent = true;
+            printf( "    ## New Property - " );
+        }
+        if ( in_PropIdent && ! isupper(current_char) ) {
+            in_PropIdent = false;
+            PropIdent[l] = '\0';
+            l = 0;
+            printf( "%s\n", PropIdent );
         }
         if ( current_char == '[' ) {
-            printf( "      # New PropValue\n" );
+            printf( "      # New PropValue start - " );
+            in_PropValue = true;
+
+            last_char = current_char;
+            continue;
+        }
+        if ( current_char == ']' ) {
+            in_PropValue = false;
+            PropValue[l] = '\0';
+            l = 0;
+            printf( "%s\n", PropValue );
+
+            last_char = current_char;
+            continue;
+        }
+
+
+        if ( in_PropValue ) {
+            PropValue[l++] = current_char;
+        }
+        if ( in_PropIdent ) {
+            PropIdent[l++] = current_char;
         }
 
         last_char = current_char;
