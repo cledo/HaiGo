4e4484d1bc7d27ce150f157e4b284fc96e6c92b9
Clemens Dorner <clemens.dorner@gmail.com>
2012-02-16 11:50:34 +0100

    builing sgf tree

--- src/sgf.c
+++ src/sgf.c
@@ -5,6 +5,14 @@
 #include "global_const.h"
 #include "sgf.h"
 
+struct node {
+    int number;
+    int tree_nr;
+    int tree_level;
+    struct node *parent;
+};
+
+void add_node( struct node *sgf_tree_start, int node_nr, int game_tree_nr, int game_tree_level );
 
 /**
  * @brief       Parses an SGF string
@@ -22,6 +30,7 @@ void parse_sgf( char *file_content )
     int  l = 0;
     char current_char;
     char last_char = '\0';
+    int  game_tree_nr    = INVALID;
     int  game_tree_level = INVALID;
     int  node_nr         = INVALID;
     bool in_PropValue    = false;
@@ -29,8 +38,24 @@ void parse_sgf( char *file_content )
     bool in_PropIdent    = false;
     char PropIdent[128];
 
+    int    node_count = 0;
+    struct node *sgf_tree;
+    struct node *sgf_tree_start;
+
+    // Count number of ';' characters to get maximum number of possible nodes:
+    while ( ( current_char = file_content[k++] ) != '\0' ) {
+        if ( current_char == ';' ) {
+            node_count++;
+        }
+    }
+    k = 0;
+
+    sgf_tree       = malloc( sizeof( struct node ) * ( node_count ) );
+    sgf_tree_start = sgf_tree;
+
     while ( ( current_char = file_content[k++] ) != '\0' ) {
         if ( current_char == '(' ) {
+            game_tree_nr++;
             game_tree_level++;
             printf( "# New GameTree %d\n", game_tree_level );
         }
@@ -40,7 +65,11 @@ void parse_sgf( char *file_content )
         }
         if ( current_char == ';' ) {
             node_nr++;
+            if ( node_nr > 0 ) {
+                sgf_tree++;
+            }
             printf( "#### New Node %d\n", node_nr );
+            add_node( sgf_tree_start, node_nr, game_tree_nr, game_tree_level );
         }
         if ( isupper(current_char) && ( isspace(last_char) || (last_char == ']') || (last_char == ';') ) ) {
             in_PropIdent = true;
@@ -78,6 +107,71 @@ void parse_sgf( char *file_content )
         }
 
         last_char = current_char;
+    }
+
+    // DEBUG:
+    for ( k = 0; k <= node_nr; k++ ) {
+        printf( "# Node Nr.: %d\n", (sgf_tree_start + k)->number );
+        printf( "#        Level: %d\n", (sgf_tree_start + k)->tree_level );
+        printf( "#        Tree : %d\n", (sgf_tree_start + k)->tree_nr );
+        if ( (sgf_tree_start + k)->parent != NULL ) {
+            printf( "#        Par  : %d\n", (sgf_tree_start + k)->parent->number );
+        }
+        else {
+            printf( "#        Par  : ??\n" );
+        }
+    }
+
+    sgf_tree = sgf_tree_start;
+    free(sgf_tree);
+    sgf_tree       = NULL;
+    sgf_tree_start = NULL;
+
+    return;
+}
+
+/**
+ * @brief       Adds new node to SGF tree
+ *
+ * Adds a new node to the SGF tree. Sets the node's number, tree level, tree
+ * number, and parent node.
+ *
+ * @param[in]   *sgf_tree_start     Starting pointer to SGF tree
+ * @param[in]   node_nr             Number of node to add
+ * @param[in]   game_tree_nr        Number of game tree this node belongs to
+ * @param[in]   game_tree_level     Level of game tree this node belongs to
+ * @return      Nothing
+ * @note        game_tree_nr and game_tree_level are important to determine
+ *              the node's parent.
+ */
+void add_node( struct node *sgf_tree_start, int node_nr, int game_tree_nr, int game_tree_level )
+{
+    int m;
+    struct node *new_node;
+
+    new_node = sgf_tree_start + node_nr;
+
+    new_node->number     = node_nr;
+    new_node->tree_nr    = game_tree_nr;
+    new_node->tree_level = game_tree_level;
+
+    // Set parent node:
+    if ( node_nr == 0 ) {
+        new_node->parent = new_node;
+    }
+    else if ( game_tree_level == (new_node - 1)->tree_level && game_tree_nr == (new_node - 1)->tree_nr ) {
+        new_node->parent = (new_node - 1);
+    }
+    else if ( game_tree_level - 1 == (new_node - 1)->tree_level && game_tree_nr - 1 == (new_node - 1)->tree_nr ) {
+        new_node->parent = (new_node - 1);
+    }
+    else {
+        for ( m = node_nr - 1; m >= 0; m-- ) {
+            if ( (sgf_tree_start + m)->tree_level == game_tree_level - 1 ) {
+                new_node->parent = (sgf_tree_start + m);
+                break;
+            }
+        }
     }
 
     return;
