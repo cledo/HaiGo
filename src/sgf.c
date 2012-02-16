ed96c5393658bacda16ff6157be473c3ccab8361
Clemens Dorner <clemens.dorner@gmail.com>
2012-02-16 12:36:54 +0100

    determining main line in sgf

--- src/sgf.c
+++ src/sgf.c
@@ -6,13 +6,14 @@
 #include "sgf.h"
 
 struct node {
-    int number;
-    int tree_nr;
-    int tree_level;
+    int  number;
+    bool is_main;
+    int  tree_nr;
+    int  tree_level;
     struct node *parent;
 };
 
-void add_node( struct node *sgf_tree_start, int node_nr, int game_tree_nr, int game_tree_level );
+void add_node( struct node *sgf_tree_start, int node_nr, int game_tree_nr, int game_tree_level, bool is_main_line );
 
 /**
  * @brief       Parses an SGF string
@@ -41,6 +42,7 @@ void parse_sgf( char *file_content )
     int    node_count = 0;
     struct node *sgf_tree;
     struct node *sgf_tree_start;
+    bool   is_main_line = true;
 
     // Count number of ';' characters to get maximum number of possible nodes:
     while ( ( current_char = file_content[k++] ) != '\0' ) {
@@ -57,32 +59,33 @@ void parse_sgf( char *file_content )
         if ( current_char == '(' ) {
             game_tree_nr++;
             game_tree_level++;
-            printf( "# New GameTree %d\n", game_tree_level );
+            //printf( "# New GameTree %d\n", game_tree_level );
         }
         if ( current_char == ')' ) {
-            printf( "# End GameTree %d\n", game_tree_level );
+            //printf( "# End GameTree %d\n", game_tree_level );
             game_tree_level--;
+            is_main_line = false;
         }
         if ( current_char == ';' ) {
             node_nr++;
             if ( node_nr > 0 ) {
                 sgf_tree++;
             }
-            printf( "#### New Node %d\n", node_nr );
-            add_node( sgf_tree_start, node_nr, game_tree_nr, game_tree_level );
+            //printf( "#### New Node %d\n", node_nr );
+            add_node( sgf_tree_start, node_nr, game_tree_nr, game_tree_level, is_main_line );
         }
         if ( isupper(current_char) && ( isspace(last_char) || (last_char == ']') || (last_char == ';') ) ) {
             in_PropIdent = true;
-            printf( "    ## New Property - " );
+            //printf( "    ## New Property - " );
         }
         if ( in_PropIdent && ! isupper(current_char) ) {
             in_PropIdent = false;
             PropIdent[l] = '\0';
             l = 0;
-            printf( "%s\n", PropIdent );
+            //printf( "%s\n", PropIdent );
         }
         if ( current_char == '[' ) {
-            printf( "      # New PropValue start - " );
+            //printf( "      # New PropValue start - " );
             in_PropValue = true;
 
             last_char = current_char;
@@ -92,7 +95,7 @@ void parse_sgf( char *file_content )
             in_PropValue = false;
             PropValue[l] = '\0';
             l = 0;
-            printf( "%s\n", PropValue );
+            //printf( "%s\n", PropValue );
 
             last_char = current_char;
             continue;
@@ -110,8 +113,10 @@ void parse_sgf( char *file_content )
     }
 
     // DEBUG:
+    /*
     for ( k = 0; k <= node_nr; k++ ) {
         printf( "# Node Nr.: %d\n", (sgf_tree_start + k)->number );
+        printf( "#        Main: %d\n", (sgf_tree_start + k)->is_main );
         printf( "#        Level: %d\n", (sgf_tree_start + k)->tree_level );
         printf( "#        Tree : %d\n", (sgf_tree_start + k)->tree_nr );
         if ( (sgf_tree_start + k)->parent != NULL ) {
@@ -121,6 +126,7 @@ void parse_sgf( char *file_content )
             printf( "#        Par  : ??\n" );
         }
     }
+    */
 
     sgf_tree = sgf_tree_start;
     free(sgf_tree);
@@ -140,11 +146,12 @@ void parse_sgf( char *file_content )
  * @param[in]   node_nr             Number of node to add
  * @param[in]   game_tree_nr        Number of game tree this node belongs to
  * @param[in]   game_tree_level     Level of game tree this node belongs to
+ * @param[in]   is_main_line        Flag that shows if this move is part of the the main line
  * @return      Nothing
  * @note        game_tree_nr and game_tree_level are important to determine
  *              the node's parent.
  */
-void add_node( struct node *sgf_tree_start, int node_nr, int game_tree_nr, int game_tree_level )
+void add_node( struct node *sgf_tree_start, int node_nr, int game_tree_nr, int game_tree_level, bool is_main_line )
 {
     int m;
     struct node *new_node;
@@ -154,6 +161,7 @@ void add_node( struct node *sgf_tree_start, int node_nr, int game_tree_nr, int g
     new_node->number     = node_nr;
     new_node->tree_nr    = game_tree_nr;
     new_node->tree_level = game_tree_level;
+    new_node->is_main    = is_main_line;
 
     // Set parent node:
     if ( node_nr == 0 ) {
--- t/75_gtp_reg_commands.t
+++ t/75_gtp_reg_commands.t
@@ -11,9 +11,19 @@ use TLib qw( get_output ok_command get_pid );
 
 my $pid = get_pid();
 
+my $sgf = <<"END_SGF";
+(
+;FF[4]GM[1]SZ[19] GN[Copyright goproblems.com] PB[Black] HA[0] PW[White] KM[5.5] DT[1999-07-21] TM[1800] RU[Japanese]
+;AW[bb][cb][cc][cd][de][df][cg][ch][dh][ai][bi][ci]AB[ba][ab][ac][bc][bd][be][cf][bg][bh]C[Black to play and live.]
+( ;B[af] ;W[ah] ( ;B[ce] ;W[ag]C[only one eye this way]) ( ;B[ag] ;W[ce])) ( ;B[ah] ;W[af] ( ;B[ae] ;W[bf] ;B[ag] ;W[bf]
+( ;B[af] ;W[ce]C[oops! you can't take this stone]) ( ;B[ce] ;W[af] ;B[bg]C[RIGHT black plays under the stones and lives]))
+( ;B[bf] ;W[ae])) ( ;B[ae] ;W[ag]))
+END_SGF
+
 my $sgf_file = "testfile.sgf";
 open my $fh, '>', $sgf_file
     or carp "cannot open testfile";
+print {$fh} $sgf;
 close $fh
     or carp "cannot close testfile";
 
