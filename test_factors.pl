55d181fd10c843c1044911b2fcb60834950ebfc7
Clemens Dorner <clemens.dorner@gmail.com>
2012-03-16 01:04:52 +0100

    more tests with test_factors

--- test_factors.pl
+++ test_factors.pl
@@ -4,8 +4,15 @@ use strict;
 use warnings;
 use Carp;
 
+use lib qw( SC_Module );
+
 use IPC::Open3;
 use Time::HiRes qw( gettimeofday tv_interval );
+
+use SC;
+
+my $table = 'table.sc';
+my $sc    = SC->new( { FILE => $table } );
 
 my ( $stdin, $stdout, $stderr );
 
@@ -13,7 +20,7 @@ my ( $stdin, $stdout, $stderr );
 #my @factor       = qw( 50 50 0 0 0 0 0 0 );
 #my @factor       = qw( 75 20 0 0 0 0 0 5 );
 #my @factor       = qw( 10 0 0 0 0 0 0 0 );
-my @factor       = qw( 1000 0 0 0 0 0 0 0 );
+my @factor       = qw( 100 50 25 5 10 2 1 1 );
 my $test_count   = 0;
 my $ok_count     = 0;
 my $not_ok_count = 0;
@@ -25,9 +32,13 @@ my $pid = open3( $stdin, $stdout, $stderr
 
 print 'Factors ' . join q{ }, @factor;
 print "\n";
+my $count = 0;
 while ( my $line = <$stdout> ) {
     if ( $line =~ qr{^# File: (\S+) (\S+) (\S+)} ) {
-        print "Result for file $1: $2 ($3s)\n";
+        print "File $1: $2 ($3s)\n";
+        $sc->set_cell( chr( $count + 65 ) . '1', "$2/$3", 'rightstring' );
+        print '# ' . $count . ': ' . chr( $count + 65 ) . "\n";
+        $count++;
         if ($2) {
             $ok_count++;
         }
@@ -35,6 +46,8 @@ while ( my $line = <$stdout> ) {
             $not_ok_count++;
         }
         $test_count++;
+
+        last if $count == 26;
     }
 }
 
@@ -45,3 +58,6 @@ print "Tests: $test_count\n";
 print "OK:    $ok_count\n";
 print "Fail:  $not_ok_count\n";
 print "Time:  $diff_time sec\n";
+
+$sc->write_file();
+
